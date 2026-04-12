37d6111996f7182bd3bf07ca4fe5be1d3aed0d90
Shaurya-Singh-18 <shauryanew119@gmail.com>
2026-04-13 01:38:47 +0530

    Fix Firebase config for Vercel deployment

--- frontend/src/firebase/firebase.config.js
+++ frontend/src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_Auth_Domain,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDERID,
-  appId: import.meta.env.VITE_APPID
+  apiKey: "AIzaSyDF9zpQ-Ic3IMOVagdPDy0GJ4tX03Bu9vk",
+  authDomain: "book-store-mern-app-2a7d8.firebaseapp.com",
+  projectId: "book-store-mern-app-2a7d8",
+  storageBucket: "book-store-mern-app-2a7d8.firebasestorage.app",
+  messagingSenderId: "318826611286",
+  appId: "1:318826611286:web:61ac4ef5689bf86d55b1f7"
 };
 
 // Initialize Firebase
